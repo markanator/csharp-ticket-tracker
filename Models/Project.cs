5d772815c31ca59933999932e820e4a2983c7b98
Mark Ambrocio <16071902+markanator@users.noreply.github.com>
2022-02-08 23:07:26 -0700

    project & proj prior model created

---
+++ Models/Project.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TheBugTracker.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Company")]
+        public int? CompanyId { get; set; }
+
+        [Required]
+        [StringLength(80)]
+        [DisplayName("Project Name")]
+        public string Name { get; set; }
+
+        [DisplayName("Description")]
+        public string Description { get; set; }
+
+        [DisplayName("Start Date")]
+        public DateTimeOffset StartDate { get; set; }
+
+        [DisplayName("End Date")]
+        public DateTimeOffset EndDate { get; set; }
+
+        [DisplayName("Priority")]
+        public int? ProjectPriorityId { get; set; }
+
+        [DisplayName("File Name")]
+        public string ImageFileName { get; set; }
+
+        [DisplayName("File Extension")]
+        public string ImageContentType { get; set; }
+
+        public byte[] ImageFileData { get; set; }
+
+        [NotMapped]
+        [DataType(DataType.Upload)]
+        public IFormFile ImageFormFile { get; set; }
+
+        [DisplayName("Archived")]
+        public bool Archived { get; set; }
+
+        // navigation properties
+        public virtual Company Company { get; set; }
+        public virtual ProjectPriority ProjectPriority { get; set; }
+
+        public virtual ICollection<BTUser> Members { get; set; } = new HashSet<BTUser>();
+        public virtual ICollection<Ticket> Tickets { get; set; } = new HashSet<Ticket>();
+    }
+}
---
+++ Models/ProjectPriority.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace TheBugTracker.Models
+{
+    public class ProjectPriority
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Priority Name")]
+        public string Name { get; set; }
+    }
+}
