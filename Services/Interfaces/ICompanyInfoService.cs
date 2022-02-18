c4e4dea988e4f50291f74f26f9d44dab4a3e8095
Mark Ambrocio <16071902+markanator@users.noreply.github.com>
2022-02-17 23:15:11 -0700

    create company Info Service

---
+++ Services/Interfaces/ICompanyInfoService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TheBugTracker.Models;
+
+namespace TheBugTracker.Services.Interfaces
+{
+    public interface ICompanyInfoService
+    {
+        Task<Company> GetCompanyInfoByIdAsync(int? companyId);
+        Task<List<BTUser>> GetAllMembersAsync(int companyId);
+        Task<List<Project>> GetAllProjectsAsync(int companyId);
+        Task<List<Ticket>> GetAllTicketsAsync(int companyId);
+    }
+}
