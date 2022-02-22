4f2d50845bf6419aab48be5d4fc750bdb9ed242e
Mark Ambrocio <16071902+markanator@users.noreply.github.com>
2022-02-21 17:41:51 -0700

    Custom VM,V,C for Assigning members to project

---
+++ ViewModels/ProjectMembersViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using TheBugTracker.Models;
+
+namespace TheBugTracker.ViewModels
+{
+	public class ProjectMembersViewModel
+	{
+		public Project Project { get; set; }
+		public SelectList Members { get; set; }
+		public List<string> SelectedMembers { get; set; }
+	}
+}
