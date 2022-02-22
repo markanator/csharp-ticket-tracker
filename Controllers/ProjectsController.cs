e9e114e668e7b87af89f4d857ef1f8f7cd0884c3
Mark Ambrocio <16071902+markanator@users.noreply.github.com>
2022-02-21 22:53:57 -0700

    auth lockdown for projects

--- Controllers/ProjectsController.cs
+++ Controllers/ProjectsController.cs
@@ -69,7 +69,45 @@ namespace TheBugTracker.Controllers
             return View(projects);
         }
 
+        // GET: Projects/UnassignedProjects
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> UnassignedProjects()
+        {
+            var projects = await _projectService.GetUnassignedProjectsAsync(User.Identity.GetCompanyId().Value);
+
+            return View(projects);
+        }
+
+        // Get: Projects/AssignPM
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> AssignPM(int id)
+        {
+            AssignPMViewModel model = new();
+            model.Project = await _projectService.GetProjectByIdAsync(id, User.Identity.GetCompanyId().Value);
+            model.ProjectManagerList = new SelectList(await _rolesService.GetUsersInRoleAsync(nameof(Roles.ProjectManager), User.Identity.GetCompanyId().Value), "Id", "FullName");
+
+            return View(model);
+        }
+
+        // Get: Projects/AssignPM
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AssignPM(AssignPMViewModel model)
+        {
+            if (!string.IsNullOrEmpty(model.PMID))
+            {
+                await _projectService.AddProjectManagerAsync(model.PMID, model.Project.Id);
+
+                return RedirectToAction(nameof(Details), new { id = model.Project.Id });
+            }
+
+            return RedirectToAction(nameof(AssignPM), new { id = model.Project.Id });
+        }
+
+
         // GET: /Projects/AssignMembers
+        [Authorize(Roles = "Admin,ProjectManager")]
         public async Task<IActionResult> AssignMembers(int id)
         {
             var model = new ProjectMembersViewModel();
@@ -88,6 +126,7 @@ namespace TheBugTracker.Controllers
         }
 
         // POST: /Projects/AssignMembers
+        [Authorize(Roles = "Admin,ProjectManager")]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> AssignMembers(ProjectMembersViewModel model)
@@ -140,6 +179,7 @@ namespace TheBugTracker.Controllers
         }
 
         // GET: Projects/Create
+        [Authorize(Roles = "Admin, ProjectManager")]
         public async Task<IActionResult> Create()
         {
             int companyId = User.Identity.GetCompanyId().Value;
@@ -153,7 +193,7 @@ namespace TheBugTracker.Controllers
         }
 
         // POST: Projects/Create
-        [HttpPost]
+        [HttpPost, Authorize(Roles = "Admin, ProjectManager")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(AddProjectWithPMViewModel model)
         {
@@ -191,6 +231,7 @@ namespace TheBugTracker.Controllers
         }
 
         // GET: Projects/Edit/5
+        [Authorize(Roles = "Admin, ProjectManager")]
         public async Task<IActionResult> Edit(int? id)
         {
             int companyId = User.Identity.GetCompanyId().Value;
@@ -206,7 +247,7 @@ namespace TheBugTracker.Controllers
         }
 
         // POST: Projects/Edit/5
-        [HttpPost]
+        [HttpPost, Authorize(Roles = "Admin, ProjectManager")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(AddProjectWithPMViewModel model)
         {
@@ -250,6 +291,7 @@ namespace TheBugTracker.Controllers
         }
 
         // GET: Projects/Delete/5
+        [Authorize(Roles = "Admin,ProjectManager")]
         public async Task<IActionResult> Archive(int? id)
         {
             if (id == null)
@@ -268,6 +310,7 @@ namespace TheBugTracker.Controllers
         }
 
         // POST: Projects/Delete/5
+        [Authorize(Roles = "Admin,ProjectManager")]
         [HttpPost, ActionName("Archive")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> ArchiveConfirmed(int id)
@@ -281,6 +324,7 @@ namespace TheBugTracker.Controllers
         }
 
         // GET: Projects/Restore/5
+        [Authorize(Roles = "Admin,ProjectManager")]
         public async Task<IActionResult> Restore(int? id)
         {
             if (id == null)
@@ -299,6 +343,7 @@ namespace TheBugTracker.Controllers
         }
 
         // POST: Projects/Restore/5
+        [Authorize(Roles = "Admin,ProjectManager")]
         [HttpPost, ActionName("Restore")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> RestoreConfirmed(int id)
@@ -307,41 +352,6 @@ namespace TheBugTracker.Controllers
             await _projectService.RestoreProjectAsync(project);
 
             return RedirectToAction(nameof(Index));
-        }
-
-        // POST: Projects/UnassignedProjects
-        [HttpGet]
-        public async Task<IActionResult> UnassignedProjects()
-        {
-            var projects = await _projectService.GetUnassignedProjectsAsync(User.Identity.GetCompanyId().Value);
-
-            return View(projects);
-        }
-
-        // Get: Projects/AssignPM
-        [HttpGet]
-        public async Task<IActionResult> AssignPM(int id)
-        {
-            AssignPMViewModel model = new();
-            model.Project = await _projectService.GetProjectByIdAsync(id, User.Identity.GetCompanyId().Value);
-            model.ProjectManagerList = new SelectList(await _rolesService.GetUsersInRoleAsync(nameof(Roles.ProjectManager), User.Identity.GetCompanyId().Value), "Id", "FullName");
-
-            return View(model);
-        }
-
-        // Get: Projects/AssignPM
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> AssignPM(AssignPMViewModel model)
-        {
-            if (!string.IsNullOrEmpty(model.PMID))
-            {
-                await _projectService.AddProjectManagerAsync(model.PMID, model.Project.Id);
-
-                return RedirectToAction(nameof(Details), new { id = model.Project.Id });
-            }
-
-            return RedirectToAction(nameof(AssignPM), new { id = model.Project.Id });
         }
 
         private async Task<bool> ProjectExists(int id)
