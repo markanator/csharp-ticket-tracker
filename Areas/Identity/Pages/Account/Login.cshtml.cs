5df68eaa3882185d6d3e568d9b89a65561ab8cd0
Mark Ambrocio <16071902+markanator@users.noreply.github.com>
2022-02-22 00:45:02 -0700

    fix typos

--- Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -59,7 +59,7 @@ namespace TheBugTracker.Areas.Identity.Pages.Account
                 ModelState.AddModelError(string.Empty, ErrorMessage);
             }
 
-            returnUrl ??= Url.Content("~/");
+            returnUrl ??= Url.Content("~/Home/Dashboard");
 
             // Clear the existing external cookie to ensure a clean login process
             await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
