751345fd9881c008083e3caa1f40930a4f52c404
Mark Ambro <wowmarkambro@gmail.com>
2022-10-18 01:15:10 -0600

    Update to NET6

--- Program.cs
+++ Program.cs
@@ -1,26 +1,77 @@
-using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System.Threading.Tasks;
 using TheBugTracker.Data;
+using TheBugTracker.Models;
+using TheBugTracker.Services;
+using TheBugTracker.Services.Factories;
+using TheBugTracker.Services.Interfaces;
 
-namespace TheBugTracker
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// STARTUP STUFF
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(DataUtils.GetConnectionString(builder.Configuration),
+                                                    // enable splitting of queries to improve perf, & prevent redundant data fetching
+                                                    o => o.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery)));
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+// Adding Custom Auth + Roles
+builder.Services.AddIdentity<BTUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+        .AddEntityFrameworkStores<ApplicationDbContext>()
+        .AddClaimsPrincipalFactory<BTUserClaimsPrincipleFactory>() // used to attach companyId to identity
+        .AddDefaultUI()
+        .AddDefaultTokenProviders();
+
+// add custom services
+builder.Services.AddScoped<IRoleService, RoleService>();
+builder.Services.AddScoped<ICompanyInfoService, CompanyInfoService>();
+builder.Services.AddScoped<IProjectService, ProjectService>();
+builder.Services.AddScoped<ITicketService, TicketService>();
+builder.Services.AddScoped<ITicketHistoryService, TicketHistoryService>();
+builder.Services.AddScoped<INotificationService, NotificationService>();
+builder.Services.AddScoped<IInviteService, InviteService>();
+builder.Services.AddScoped<IFileService, FileService>();
+builder.Services.AddScoped<ILookupService, LookupService>();
+
+builder.Services.AddScoped<IEmailSender, EmailService>();
+builder.Services.Configure<MailSettings>(builder.Configuration.GetSection("MailSettings"));
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+// SEED
+await DataUtils.ManageDataAsync(app);
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var host = CreateHostBuilder(args).Build();
-            // seed DB
-            await DataUtils.ManageDataAsync(host);
-            // run MVC App
-            host.Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+} else {
+    app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+;
+app.MapRazorPages();
+
+app.Run();
